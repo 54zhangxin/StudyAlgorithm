e1b85b0f86849e58533e806fe159c30ebc43dfe1
zhangxin <540026013@qq.com>
2022-09-23 17:10:19 +0800

    合并区间

--- src/合并区间.java
+++ src/合并区间.java
@@ -1,15 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * https://leetcode.cn/problems/merge-intervals/
  * 给出一个区间的集合，请合并所有重叠的区间。
  * 输入: intervals = [[1,3],[2,6],[8,10],[15,18]]
  * 输出: [[1,6],[8,10],[15,18]]
  * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6]。
- *
+ * <p>
  * medium  数组 排序
  */
 public class 合并区间 {
     public int[][] merge(int[][] intervals) {
-
+        List<int[]> res = new ArrayList<>();
+        if (intervals.length == 0 || intervals == null) {
+            return res.toArray(new int[0][]);
+        }
+        // 对起点终点进行排序
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] interval1, int[] interval2) {
+                return interval1[0] - interval2[0];
+            }
+        });
+        int i = 0;
+        while (i < intervals.length) {
+            int left = intervals[i][0];
+            int right = intervals[i][1];
+            // 如果有重叠，循环判断哪个起点满足条件
+            while (i < intervals.length - 1 && intervals[i + 1][0] <= right) {
+                i++;
+                right = Math.max(right, intervals[i][1]);
+            }
+            // 将现在的区间放进res里面
+            res.add(new int[]{left, right});
+            // 接着判断下一个区间
+            i++;
+        }
+        return res.toArray(new int[0][]);
     }
 
 }

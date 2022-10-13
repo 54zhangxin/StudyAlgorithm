945884174928b59db84433ba10eefeb7fa0c4511
zhangxin <540026013@qq.com>
2022-10-13 15:51:09 +0800

    update 二叉树

--- src/二叉树/二叉树的层序遍历.java
+++ src/二叉树/二叉树的层序遍历.java
@@ -8,9 +8,12 @@ import java.util.List;
 /**
  * medium
  * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
- * 用队列实现
+ * 可参考题解：
+ * https://leetcode.cn/problems/binary-tree-level-order-traversal/solution/die-dai-di-gui-duo-tu-yan-shi-102er-cha-shu-de-cen/
  */
 public class 二叉树的层序遍历 {
+
+    // 广度优先搜索 BFS 用队列实现
     public List<List<Integer>> levelOrder(TreeNode root) {
         if (root == null) {
             return new ArrayList<List<Integer>>();
@@ -41,4 +44,47 @@ public class 二叉树的层序遍历 {
 
         return res;
     }
+
+
+    // DFS 深度优先遍历
+    /**
+     *      1
+     *   2    3
+     * 4  5  6  7
+     * 深度 1 2 4 5 3 6 7
+     * 当成梯田
+     *  1
+     *  2 3
+     *  4 5 6 7
+     *  每次递归的时候都需要带一个 index(表示当前的层数)，也就对应那个田字格子中的第几行，如果当前行对应的 list 不存在，就加入一个空 list 进去。
+     *
+     */
+
+    public List<List<Integer>> levelOrderByDFS(TreeNode root) {
+        if(root==null) {
+            return new ArrayList<List<Integer>>();
+        }
+        //用来存放最终结果
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        dfs(1,root,res);
+        return res;
+    }
+
+    void dfs(int index,TreeNode root, List<List<Integer>> res) {
+        //假设res是[ [1],[2,3] ]， index是3，就再插入一个空list放到res中
+        if(res.size()<index) {
+            res.add(new ArrayList<Integer>());
+        }
+        //将当前节点的值加入到res中，index代表当前层，假设index是3，节点值是99
+        //res是[ [1],[2,3] [4] ]，加入后res就变为 [ [1],[2,3] [4,99] ]
+        res.get(index-1).add(root.val);
+        //递归的处理左子树，右子树，同时将层数index+1
+        if(root.left!=null) {
+            dfs(index+1, root.left, res);
+        }
+        if(root.right!=null) {
+            dfs(index+1, root.right, res);
+        }
+    }
+
 }
--- src/链表/环形链表.java
+++ src/链表/环形链表.java
@@ -3,16 +3,26 @@ package 链表;
 /**
  * https://leetcode.cn/problems/linked-list-cycle/
  * easy
- * 快慢指针
+ * 思路：快慢指针
+ *
+ * 算法步骤：
+ * 1、慢指针和快指针两个指针初始化均指向头结点
+ * 2、循环遍历慢指针和快指针，每次让慢指针移动一步，快指针移动两步，当一个链表有环时，两个指针会陷入无限循环中，然后变成了追及问题，
+ *    只要一直移动下去，快指针总会追上慢指针。
+ * 3、如果 快指针==慢指针 即结束循环
+ *
+ * 复杂度分析：
+ * 时间复杂度 O(N)：N 为链表节点数量
+ * 空间复杂度 O(1)：占用常数大小的空间
  */
 public class 环形链表 {
 
     public boolean hasCycle(ListNode node) {
-
         ListNode fastNode = node;
         ListNode slowNode = node;
 
         while (node.next != null) {
+            // 如果就一个节点  那肯定就不是环形链表
             if (fastNode.next == null) {
                 return false;
             }

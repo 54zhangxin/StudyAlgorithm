775ba003b800cd941deaccdd1d724591fa392080
zhangxin <540026013@qq.com>
2023-01-12 16:08:09 +0800

    update 滑动窗口最大值

--- src/滑动窗口/滑动窗口最大值.java
+++ src/滑动窗口/滑动窗口最大值.java
@@ -1,5 +1,6 @@
 package 滑动窗口;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 /**
@@ -7,26 +8,45 @@ import java.util.LinkedList;
  * hard
  * 给你一个整数数组 nums，有一个大小为k的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k个数字。滑动窗口每次只向右移动一位。
  * <p>
- * 返回 滑动窗口中的最大值 。
+ * 返回 滑动窗口中的最大值 。 // 滑动窗口中的最大值 并不是单个值 而是一个数组
+ * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * 输出：[3,3,5,5,6,7]
+ * 解释：
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ *  1 [3  -1  -3] 5  3  6  7       3
+ *  1  3 [-1  -3  5] 3  6  7       5
+ *  1  3  -1 [-3  5  3] 6  7       5
+ *  1  3  -1  -3 [5  3  6] 7       6
+ *  1  3  -1  -3  5 [3  6  7]      7
+
  */
 public class 滑动窗口最大值 {
     //https://leetcode.cn/problems/sliding-window-maximum/solution/shuang-xiang-dui-lie-jie-jue-hua-dong-chuang-kou-2/
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
         if (nums == null || nums.length < 2) return nums;
         // 双向队列 保存当前窗口最大值的数组位置 保证队列中数组位置的数值按从大到小排序
-        LinkedList<Integer> queue = new LinkedList();
+        Deque<Integer> queue = new LinkedList();
         // 结果数组
         int[] result = new int[nums.length - k + 1];
         // 遍历nums数组
         for (int i = 0; i < nums.length; i++) {
             // 保证从大到小 如果前面数小则需要依次弹出，直至满足要求
+            // queue.peekLast() 返回尾部元素
+            // 队尾元素小于当前值的话要依次弹出 放入当前最大值
             while (!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]) {
+                //检索并删除LinkedList的最后一个元素
                 queue.pollLast();
             }
             // 添加当前值对应的数组下标
             queue.addLast(i);
             // 判断当前队列中队首的值是否有效
+            // peek() 返回第一个元素。
+            // 队列中的第一个元素是滑动窗口左边
+            // 如果队列中已经满k个元素要剔除队首元素
             if (queue.peek() <= i - k) {
+            //检索并删除List的第一个元素。
                 queue.poll();
             }
             // 当窗口长度为k时 保存当前窗口中最大值
@@ -35,5 +55,12 @@ public class 滑动窗口最大值 {
             }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 4, 1, 4, 3, 2};
+
+        maxSlidingWindow(nums,3);
+
     }
 }

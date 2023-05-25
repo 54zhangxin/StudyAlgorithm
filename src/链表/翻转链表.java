d3ca6a673bfb6442790bc47f544aa20bf5af21c0
zhangxin <540026013@qq.com>
2023-05-25 19:09:43 +0800

    update 翻转链表

--- src/链表/翻转链表.java
+++ src/链表/翻转链表.java
@@ -5,6 +5,12 @@ package 链表;
  */
 public class 翻转链表 {
     //迭代
+
+    /**
+     *   null   a  ->  b  ->  c
+     *   pre   cur
+     *
+     */
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
@@ -30,6 +36,22 @@ public class 翻转链表 {
      * https://leetcode.cn/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
      * 递归解法 看图就容易理解了
      *
+     * reverseList: head=1
+     *     reverseList: head=2
+     * 	    reverseList: head=3
+     * 		    reverseList:head=4
+     * 			    reverseList:head=5
+     * 					终止返回
+     * 				cur = 5
+     * 				4.next.next->4，即5->4
+     * 			cur=5
+     * 			3.next.next->3，即4->3
+     * 		cur = 5
+     * 		2.next.next->2，即3->2
+     * 	cur = 5
+     * 	1.next.next->1，即2->1
+     *
+     * 	最后返回cur
      */
     public ListNode reverseList2(ListNode head) {
         if (head == null || head.next == null) {
